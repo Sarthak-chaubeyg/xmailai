9d9a8aeb4cfe74f8097db2ec7b6aed955b274d07
volta <voltavinaycadila751@gmail.com>
2026-04-19 04:12:10 +0530

    cleanup temp files

--- _fix.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "GH=C:\Program Files\GitHub CLI\gh.exe"
-cd "c:\Users\volta\web search"
-git add -A
-git commit -m "fix: blobs store and esbuild config for production"
-git push origin master
-echo DONE - EXIT CODE: %errorlevel%
